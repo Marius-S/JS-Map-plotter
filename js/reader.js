7e0e9e47c95ea96bdcf251e48e4f8ba045a91284
Marius Š <smigelskas.marius@gmail.com>
2016-10-22 18:05:17 +0300

    End of map plotting

--- js/reader.js
+++ js/reader.js
@@ -1,8 +1,14 @@
 function read() {
-  var div = document.getElementById('map');
   var div_map = document.getElementById('map-plot');
-  div.innerHTML = "";
   div_map.innerHTML = "";
+//var table = $('table.grid');
+if($('table.grid').length){
+
+    $('table.grid').remove();
+    // exists
+}
+
+
   var x_line = document.getElementById("x_size").value;
   var y_line = document.getElementById("y_size").value;
   var lines = document.getElementById("text_zone").value.split('\n');
@@ -11,24 +17,22 @@ function read() {
 
   if(lines.length > x_line * y_line - x_line){
 
-  for(var i = 0;i < lines.length;i++) {
-    var temp = lines[i];
-    temp = parseInt(temp);
-    if (arrayX.length != x_line){
-      arrayX.push(temp);
-    }
-    else{
-      arrayY.push(arrayX);
-      arrayX = [];
-      arrayX.push(temp);
+    for(var i = 0;i < lines.length;i++) {
+      var temp = lines[i];
+      temp = parseInt(temp);
+      if (arrayX.length != x_line){
+        arrayX.push(temp);
+      }
+      else{
+        arrayY.push(arrayX);
+        arrayX = [];
+        arrayX.push(temp);
+      }
     }
   }
-
-}
-else{
-  div_map.innerHTML = div_map.innerHTML + "Sorry, array too small";
-}
-
+  else{
+    div_map.innerHTML = div_map.innerHTML + "Sorry, array too small";
+  }
   if (arrayX != null){
     arrayY.push(arrayX);
   }
@@ -38,9 +42,6 @@ else{
     div_map.innerHTML = div_map.innerHTML + "You clicked on row: " + row + "<br>";
     div_map.innerHTML = div_map.innerHTML + "You clicked on col: " + col + "<br>";
     div_map.innerHTML = div_map.innerHTML + "Item value: " + arrayY[row][col];
-    el.className='clicked';
-    if (lastClicked) lastClicked.className='';
-    lastClicked = el;
   });
   document.body.appendChild(grid);
   return arrayY;
